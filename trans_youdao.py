d0dfa86ed5bc999479143d19c5d974440ad4baab
Yunjie Huo <yunjiehuo@bestechnic.com>
2017-10-10 09:28:09 +0800

    Add phonetic into translte

--- trans_youdao.py
+++ trans_youdao.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import os
 import sys
 import re
@@ -6,13 +7,37 @@ import subprocess
 
 from utils import *
 
-def trans_word(word):     
+def trans_word(word, phonetic_mode):
+    '''
+    <h2 class="wordbook-js">
+        <span class="keyword">hello</span>
+                            <div class="baav">
+                            <span class="pronounce">英
+                                    <span class="phonetic">[h??l??]</span>
+                                                    <a href="#" title="真人发音" class="sp dictvoice voice-js log-js" data-rel="hello&type=1" data-4log="dict.basic.ec.uk.voice"></a>
+                                </span>
+                                      <span class="pronounce">美
+                                    <span class="phonetic">[h??lo]</span>
+                                                    <a href="#" title="真人发音" class="sp dictvoice voice-js log-js" data-rel="hello&type=2" data-4log="dict.basic.ec.us.voice"></a>
+                                </span>
+                                  </div>
+            </h2>
+             <div class="trans-container">
+
+   <ul>
+     <li>int. 喂；哈罗</li>
+     <li>n. 表示问候， 惊奇或唤起注意时的用语</li>
+     <li>n. (Hello)人名；(法)埃洛</li>
+    </ul>
+
+    phonetic_mode: English = 0; American = -1
+    '''
     url='http://dict.youdao.com/search?q=%s'%word
-     
+
     content=urllib.urlopen(url)
 
-    pattern=re.compile("</h2.*?</ul>",re.DOTALL)
-     
+    pattern=re.compile("<h2.*?</ul>",re.DOTALL)
+
     result0 = pattern.search(content.read())
 
     if result0 == None:
@@ -20,12 +45,14 @@ def trans_word(word):
       return None
     
     result1 = result0.group()
+
     # print  result1
 
     if result1.startswith('</h2></div>') == True:
       print_error('Can not translator this word: %s'%word)
       return None
 
+    # 中文翻译
     pattern2=re.compile('<li>.*?</li>')
     trans = []
     for i in pattern2.findall(result1):
@@ -33,12 +60,29 @@ def trans_word(word):
         trans.append(i.strip('<li>').strip('</li>'))
         # file_object.write(i.strip('<li>').strip('</li>')+'\n')
 
-    return trans
+    # 音标
+    pattern3=re.compile('<span class="phonetic">.*?</span>')
 
-def structure_youdao_xml(word):
-    trans = trans_word(word)
+    phonetic = pattern3.findall(result1)[phonetic_mode].strip('<span class="phonetic">').strip('</span>')
 
-    # print len(trans)
+    return trans, phonetic
+
+def structure_youdao_xml(word, phonetic_mode):
+    '''
+        <item>    <word>test</word>
+        <trans><![CDATA[n. 试验；检验;
+    vt. 试验；测试;
+    vi. 试验；测试;
+    n. (Test)人名；(英)特斯特]]></trans>
+        <phonetic><![CDATA[[test]]]></phonetic>
+        <tags></tags>
+        <progress>1</progress>
+    </item></wordbook>
+    '''
+    trans, phonetic = trans_word(word, phonetic_mode)
+
+    # print trans, phonetic, len(phonetic)
+
     str = ''
 
     if trans == None:
@@ -59,10 +103,15 @@ def structure_youdao_xml(word):
             str += ']]></trans>'
         str += '\n'
 
+    str += '<phonetic><![CDATA[' + phonetic + ']]></phonetic>\n'
     str += '<tags></tags>\n'
     str += '<progress>1</progress>\n'
 
     return str
+
+phonetic_mode_English = 0
+phonetic_mode_American = -1
+phonetic_mode = phonetic_mode_American
 
 def trans_youdao_xml(file_words, trans_out_file, trans_error_file):
     fileHandleWords = open (file_words, 'r')
@@ -76,7 +125,7 @@ def trans_youdao_xml(file_words, trans_out_file, trans_error_file):
     for line in fileHandleWords.readlines():
         line = line.strip('\n')
         word = line.rstrip()
-        strTemp = structure_youdao_xml(word)
+        strTemp = structure_youdao_xml(word, phonetic_mode)
         if strTemp == '':
             strOther += word+'\n'
             error_num += 1
